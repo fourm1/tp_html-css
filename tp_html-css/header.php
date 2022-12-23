ecf2201341b362429061f537439da970427c51e0
Steeven Wlh <stev.73@live.fr>
2022-12-23 19:11:31 +0100

    Add files via upload

---
+++ tp_html-css/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+
+
+
+    <title>header</title>
+</head>
+
+<body>
+    <header role="header">
+
+        <nav class="menu" role="navigation">
+
+            <div class="inner">
+                <div class="m-left">
+                    <h1 id="titre"><img class="logo-img"src="../assets/images/plane.png" alt="">TRAVEL AGENCY</h1>
+                </div>
+
+                <div class="m-right">
+                    <a href="#" class="link">Home</a>
+                    <a href="#" class="link">Destinations</a>
+                    <a href="#" class="link">Deals</a>
+                    <a href="#" class="link">Contact</a>
+                </div>
+                    <div class="m-nav">
+                        <span class="icon"></span>
+                    </div>
+            </div>
+
+        </nav>
+
+    </header>
+</body>
+
+</html>
